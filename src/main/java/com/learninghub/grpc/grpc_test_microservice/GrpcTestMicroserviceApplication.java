331b491fb8899fcb3c86312435bcd49a7738e640
jahnavireddy9989 <jahnavireddy9989@gmail.com>
2025-03-15 16:48:25 +0530

    Initial commit for gRPC test microservice

---
+++ src/main/java/com/learninghub/grpc/grpc_test_microservice/GrpcTestMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.learninghub.grpc.grpc_test_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcTestMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcTestMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learninghub/grpc/grpc_test_microservice/GrpcTestMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learninghub.grpc.grpc_test_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcTestMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
